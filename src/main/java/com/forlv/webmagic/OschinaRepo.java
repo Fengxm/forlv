b241aede303e6627d8fda484f39472d28e045a08
Fengxm <fengxiaoming2010@126.com>
2016-12-23 17:31:57 +0800

    抽取oschina博客标题，摘要，标签

---
+++ src/main/java/com/forlv/webmagic/OschinaRepo.java
@@ -0,0 +1,30 @@
+package com.forlv.webmagic;
+
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.model.ConsolePageModelPipeline;
+import us.codecraft.webmagic.model.OOSpider;
+import us.codecraft.webmagic.model.annotation.ExtractBy;
+import us.codecraft.webmagic.model.annotation.HelpUrl;
+import us.codecraft.webmagic.model.annotation.TargetUrl;
+
+import java.util.List;
+
+/**
+ * Created by xiaoming on 2016/12/23.
+ */
+
+@TargetUrl("https://my.oschina.net/u/\\d+/blog/\\d+")
+@HelpUrl("https://my.oschina.net/u/\\d+/blog")
+public class OschinaRepo {
+    @ExtractBy(value = "//div[@class='title']/text()",notNull = true)
+    private String title;
+    @ExtractBy(value = "//div[@class='blog-abstract']/text()")
+    private String abstracts;
+    @ExtractBy(value = "//div[@class='tags']/span[@class='tag']/a/text()")
+    private String tags;
+
+    public static void main(String[] args) {
+        OOSpider.create(Site.me().setTimeOut(1000),new ConsolePageModelPipeline(),OschinaRepo.class).addUrl("https://www.oschina.net/blog").run();
+    }
+
+}
